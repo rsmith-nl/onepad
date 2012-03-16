15472ac6fceb9fdf9528dfb70c2238d389e84146
Roland Smith <rsmith@xs4all.nl>
2012-03-16 23:15:43 +0100

    Finish first version of onepad.py.

--- onepad.py
+++ onepad.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright © 2012 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
-# Time-stamp: <2012-03-15 23:43:13 rsmith>
+# Time-stamp: <2012-03-16 23:12:17 rsmith>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -28,12 +28,33 @@
 
 import os
 
+def readb64(name):
+    '''Read data from the base64 encoded text file 'name'.'''
+    f = open(name, 'r') # Can raise IOError.
+    buf = f.read().translate(None, ' \t\n').decode('base64')
+    f.close()
+    return bytearray(buf)
+
+def writeb64(name, data):
+    '''Write 'data' to the file 'name' in base64 encoded text.'''
+    dl = len(data)
+    if dl == 0:
+        raise IOError('Nothing to write.')
+    f = open(name, 'w+')
+    segsz = 57
+    for i in range(0, dl, segsz):
+        f.write(str(data[i:i+segsz]).encode('base64'))
+    f.close()
+    
+
 class Key(object):
 
     def __init__(self, name=None, size=None):
+        '''Read a key from a file 'name', or create a new one from os.urandom
+        that is 'size' bytes long.'''
         self.data = bytearray()
         if name:
-            self.read(name)
+            self.date = readb64(name)
             if len(self.data):
                 return
         if size == None:
@@ -45,26 +66,12 @@ class Key(object):
     def __len__(self):
         return len(self.data)
 
-    def read(self, name):
-        f = open(name, 'r') # Can raise IOError.
-        buf = f.read().translate(None, ' \t\n').decode('base64')
-        f.close()
-        if len(self.data):
-            del self.data
-        self.data = bytearray(buf)
-        del buf
-
     def write(self, name):
-        dl = len(self.data)
-        if dl == 0:
-            raise IOError('Key is empty, nothing to write.')
-        f = open(name, 'w+')
-        segsz = 57
-        for i in range(0, dl, segsz):
-            f.write(str(self.data[i:i+segsz]).encode('base64'))
-        f.close()
+        '''Write the key to a file 'name' in base64 encoding.'''
+        writeb64(name, self.data)
 
-    def use(self, message):
+    def crypt(self, message):
+        '''Encrypt or decrypt 'message'.'''
         a = len(message)
         rv = bytearray([p^q for p,q in zip(bytearray(message),self.data)])
         if len(self.data) > a:
@@ -73,6 +80,18 @@ class Key(object):
             self.data = bytearray()
         return rv
 
+
 # Built-in test.
 if __name__ == '__main__':
-    pass
+    from copy import deepcopy
+    k = Key(None, 1024)
+    p = deepcopy(k)
+    test = 'This is a test.'
+    ciphertext = k.crypt(test)
+    copy  =  str(p.crypt(ciphertext))
+    if copy == test:
+        print "It works!"
+    else:
+        print 'original: "{}"'.format(test)
+        print 'crypted: "{}"'.format(copy)
+
