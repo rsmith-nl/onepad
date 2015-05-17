86e62749f269debe55d4d7dbe7d50dbdd17cbaee
Roland Smith <rsmith@xs4all.nl>
2015-05-17 11:35:07 +0200

    Made test script return a value.

--- test/test.sh
+++ test/test.sh
@@ -4,15 +4,18 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-04-06 16:06:32 +0200
-# $Date$
-# $Revision$
+# Last modified: 2015-05-17 11:31:57 +0200
 
+../genpad.py foo.key
 ../onepad.py enc plain.txt foo.key >enc.txt
-../onepad.py dec enc.txt foo >res.txt
+../onepad.py dec enc.txt foo.key >res.txt
 diff -Bu plain.txt res.txt
 if [ $? -ne 0 ]; then
     echo "Test failed!"
+    rv=1
 else
     echo "Test succeeded!"
+    rv=0
 fi
-rm -f enc.txt res.txt
+rm -f enc.txt res.txt foo.key
+return $rv
