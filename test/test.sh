4261e3edd672314602faeea4a14cde9855309f09
Roland Smith <rsmith@xs4all.nl>
2015-05-17 12:05:27 +0200

    Added randomness test.

--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,15 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-04-06 16:06:32 +0200
-# Last modified: 2015-05-17 11:31:57 +0200
+# Last modified: 2015-05-17 12:04:05 +0200
 
+echo "* Testing key randomness."
+../genpad.py -l 1000 foo.key
+./ent.py foo.key
+rm -f foo.key
+
+echo
+echo "* Testing encryption/decryption."
 ../genpad.py foo.key
 ../onepad.py enc plain.txt foo.key >enc.txt
 ../onepad.py dec enc.txt foo.key >res.txt
